7cb7bebc8b128dea72a9d44806cb70cc25da091f
Robert Grantham <robertgrantham40@gmail.com>
2020-08-22 00:35:41 -0500

    Update Austin Crime Reports Analysis.py

--- Crime Reports/Austin Crime Reports Analysis.py
+++ Crime Reports/Austin Crime Reports Analysis.py
@@ -33,7 +33,10 @@
 
 
 # Importing essential libraries and configurations
+from shapely import speedups
+from shapely.geometry import Polygon
 import contextily as cxt
+import geopandas as gp
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
@@ -50,18 +53,11 @@ pd.set_option('display.max_columns', None)
 # Loading the data
 df = pd.read_csv('crime_reports.csv')
 
-# Making a copy to work with later
-df2 = df.copy()
-
 
 # In[3]:
 
 
 # Examining the dataframe
-display(df.head())
-print('----------------------------------')
-display(df.tail())
-print('----------------------------------')
 display(df.info())
 print('----------------------------------')
 display(df.duplicated().sum())
@@ -76,24 +72,37 @@ display(df.isnull().sum())
 # In[4]:
 
 
-# Create a helper function to aid in data scrubbing
+# Helper function to clean the data
 def clean_data(df):
-    """Deleting unnecessary columns"""
-    drop_col = ['Occurred Date', 'Occurred Time', 'Report Date', 'Report Time', 'Census Tract', 'UCR Category', 
-                'Category Description', 'X-coordinate', 'Y-coordinate', 'Location'] 
+    drop_col = ['Occurred Date', 
+                'Occurred Time', 
+                'Report Date', 
+                'Report Time', 
+                'Census Tract', 
+                'UCR Category', 
+                'Category Description', 
+                'X-coordinate', 
+                'Y-coordinate'] 
     df.drop(drop_col, axis=1, inplace=True) 
-    """Cleaning essential columns and/or converting their datatype"""
-    clean_col = ['Occurred Date Time', 'Zip Code'] 
+    clean_col = ['Occurred Date Time', 
+                 'Zip Code', 
+                 'PRA'] 
     df.dropna(subset=clean_col, inplace=True)
     df.rename(columns=lambda x: x.strip().lower().replace(" ", "_"), inplace=True)
+    d = {'Y': True, 'N': False}
+    e = {'C': True, 'O': True, 'N': False}
+    df.clearance_status = df.clearance_status.map(e)
+    df.clearance_status = df.clearance_status.astype('bool')
+    df.family_violence = df.family_violence.map(d)
+    df.family_violence = df.family_violence.astype('bool')
     df.zip_code = df.zip_code.astype('int64')
+    df.pra = df.pra.astype('int64')
     df.clearance_date = df.clearance_date.astype('datetime64') 
     df.highest_offense_description = df.highest_offense_description.astype('category') 
     df.location_type = df.location_type.astype('category') 
     df.apd_sector = df.apd_sector.astype('category') 
     df.report_date_time = df.report_date_time.astype('datetime64') 
     df['month'] = df['report_date_time'].dt.month
-    """Setting the index"""
     df.set_index(['report_date_time'], inplace=True) 
     df.sort_index(inplace=True)
     return df
@@ -122,25 +131,43 @@ display(df.tail())
 # 
 # ***Note: I am only including zipcodes and crimes, for questions 1 - 3, that >= 1%. Any zipcodes or crime percentages, below 1%, will be discluded to simplify analysis and visualizations.***
 
-# In[10]:
+# In[21]:
+
+
+color = ['green', 'blue', 'red', 'yellow', 'purple', 'cyan', 'gold', 'black', 'pink']
 
 
 # Create and show dataframe for crime rates by zipcode
-zip_codes = df.zip_code.value_counts().head(24)
+zip_codes = df.zip_code.value_counts()
 display(zip_codes)
 
 # Showing the results as percentages
 print('----------------------------------')
-display(df.zip_code.value_counts(normalize=True).head(24))
-
-zip_codes.plot.bar(figsize=(20,10), fontsize=14, rot=60)
+df.zip_code.value_counts(normalize=True).plot.bar(figsize=(15,7), 
+                                                            rot=60)
 
 plt.xlabel('Zip Code')
-plt.ylabel('Police Reports Taken')
+plt.ylabel('Crime Percentages')
 plt.title('Crime Rate by Zipcode')
+plt.show()
+
+zip_off_desc = pd.crosstab(df.zip_code, df.highest_offense_description)
+display(zip_off_desc)                        
 
 
-# Out of all the areas in Austin, 78741 has the highest percentage of overall crime at 9.08%. This is a significant 1.3 percentage points higher than the number 2 area 78753 which hosts 7.78% of overall crime.
+# In[8]:
+
+
+zip_off_desc.to_csv('zip_off_desc.csv')
+
+
+# In[9]:
+
+
+df.highest_offense_description.value_counts(normalize=True)
+
+
+# Out of all the areas in Austin, 78741 has the highest percentage of overall crime at 9.06%. This is a significant 1.3 percentage points higher than the number 2 area 78753 which hosts 7.8% of overall crime.
 
 # #### Taking a closer look at particular areas... 
 # 
@@ -151,7 +178,7 @@ plt.title('Crime Rate by Zipcode')
 # <a id='q2'></a>
 # ### B. Question 2. How is crime distributed in 78753? 
 
-# In[11]:
+# In[10]:
 
 
 # Examining crime in the 78753 area
@@ -165,14 +192,15 @@ display(df_53_off)
 print('----------------------------------')
 display(df_53.highest_offense_description.value_counts(normalize=True).head(22))
 
-df_53_off.plot.pie(figsize=(10,10), fontsize=14, rot=60)
+df_53_off.plot.pie(fontsize=12,
+                   figsize=(8,8))
 plt.title('Crime Distribution (78753)')
 
 
 # <a id='q3'></a>
 # ### C. Question 3. How is crime distributed in 78741? 
 
-# In[12]:
+# In[11]:
 
 
 # Create a dataframe for crime in the 78741 area (the highest amount of crime of any Austin zip code)
@@ -186,14 +214,33 @@ display(df_41_off)
 print('----------------------------------')
 display(df_41.highest_offense_description.value_counts(normalize=True).head(21))
 
-df_41_off.plot.pie(figsize=(10,10), fontsize=14)
+df_41_off.plot.pie(figsize=(8,8), 
+                   fontsize=12)
 plt.title('Crime Distribution (78741)')
 
 
 # <a id='q4'></a>
 # ### D. Question 4. How are violent crimes, in particular murder, capital murder, and rape, distributed? 
-# 
-# ***Note: Only including areas where rape crimes >= 1%.***
+
+# ***The following line of code shows crime rates only >= 1% per zipcode.***
+
+# In[12]:
+
+
+df_viol = df.query('highest_offense_description == ["MURDER", "CAPITAL MURDER", "RAPE", "AGG ASSAULT"]') 
+df_viol_mur = df.query('highest_offense_description == ["MURDER", "CAPITAL MURDER"]')
+
+df_viol_zip = df_viol.zip_code.value_counts()
+display(df_viol_zip)
+display(df_viol.zip_code.value_counts(normalize=True))
+
+df_viol_zip.plot.bar(rot=60,
+                     width=0.4,
+                     figsize=(15,7),
+                     fontsize=14)
+plt.title('Violent Crime Distribution by Zipcode since 2003')
+plt.show()
+
 
 # In[13]:
 
@@ -208,10 +255,10 @@ df_agg_asslt = df[df.highest_offense_description == 'AGG ASSAULT']
 print('----------------------------------')
 print('Murder')
 print('----------------------------------')
-df_mur_val = df_mur.zip_code.value_counts().head(24)
+df_mur_val = df_mur.zip_code.value_counts()
 display(df_mur_val)
 print('----------------------------------')
-display(df_mur.zip_code.value_counts(normalize=True).head(24))
+display(df_mur.zip_code.value_counts(normalize=True))
 
 # What are the top zipcodes for capital murders? 
 print('----------------------------------')
@@ -226,115 +273,77 @@ display(df_mur_cap.zip_code.value_counts(normalize=True))
 print('----------------------------------')
 print('Rape')
 print('----------------------------------')
-df_rape_val = df_rape.zip_code.value_counts().head(21)
+df_rape_val = df_rape.zip_code.value_counts()
 display(df_rape_val)
 print('----------------------------------')
-display(df_rape.zip_code.value_counts(normalize=True).head(21))
+display(df_rape.zip_code.value_counts(normalize=True))
 
 print('----------------------------------')
 print('Aggrivated Assault')
 print('----------------------------------')
-df_agg_asslt_val = df_agg_asslt.zip_code.value_counts().head(16)
+df_agg_asslt_val = df_agg_asslt.zip_code.value_counts()
 display(df_agg_asslt_val)
 print('----------------------------------')
-display(df_agg_asslt.zip_code.value_counts(normalize=True).head(16))
+display(df_agg_asslt.zip_code.value_counts(normalize=True))
         
 
-df_mur_val.plot.bar(figsize=(20,10), rot=60, fontsize=14)
+df_mur_val.plot.bar(figsize=(15,7),
+                    rot=60,
+                    width=0.4,
+                    fontsize=12)
 plt.title('Murder')
 plt.show()
 
-df_mur_cap_val.plot.bar(figsize=(20,10), rot=60, fontsize=14)
+df_mur_cap_val.plot.bar(figsize=(15,7),
+                        rot=60, 
+                        width=0.4,
+                        fontsize=12)
 plt.title('Capital Murder')
 plt.show()
 
-df_rape_val.plot.bar(figsize=(20,10), fontsize=14, rot=60)
+df_rape_val.plot.bar(figsize=(15,7),
+                     rot=60, 
+                     width=0.4,
+                     fontsize=12)
 plt.title('Rape')
 plt.show()
 
-df_agg_asslt_val.plot.bar(figsize=(20,10), fontsize=14, rot=60)
+df_agg_asslt_val.plot.bar(figsize=(15,7),
+                          rot=60, 
+                          width=0.4,
+                          fontsize=12)
 plt.title('Aggrivated Assault')
 plt.show()
 
 
-# #### Showing Murder, Capital Murder, Rape, and Aggrivated Assault on the map...
-# 
-# ***Note: I attempted to use the Contextily library in order to add a basemap to the following plots but was unsuccessful.***
-
-# In[14]:
-
-
-df_viol = df.query('highest_offense_description == ["MURDER", "CAPITAL MURDER", "RAPE", "AGG ASSAULT"]') 
-
-df_viol_zip = df_viol.zip_code.value_counts().head(17)
-display(df_viol_zip)
-display(df_viol.zip_code.value_counts(normalize=True).head(17))
-
-df_viol_zip.plot.bar(figsize=(20,10), fontsize=14, rot=60)
-plt.title('Violent Crime Distribution since 2003')
-plt.show()
-
-fig, ax = plt.subplots(figsize=(10,10))
-ax.plot(df_viol['longitude'], df_viol['latitude'], 'o', markersize=1)
-plt.show()
-
-
-# In[45]:
-
-
-viol_month = df_viol.groupby(df_viol['month'])['highest_offense_description'].value_counts()
-display(viol_month)
-display(df_viol.groupby(df_viol['month'])['highest_offense_description'].value_counts(normalize=True))
-
-fig, axs = plt.subplots(figsize=(20,20))
-df_viol.groupby(df_viol['month'])['highest_offense_description'].value_counts().plot.barh(ax=axs)
-plt.show()
-
-
-# In[ ]:
-
-
-viol_month.to_csv('datasets\viol_month.csv')
-
-
-# In[15]:
+# In[26]:
 
 
 viol_freq = pd.crosstab(df_viol.zip_code, df_viol.highest_offense_description)
 display(viol_freq)
 
-
-# In[46]:
-
-
-viol_freq_1 = viol_freq.loc['78701':'78720']
-viol_freq_2 = viol_freq.loc['78721':'78730']
-viol_freq_3 = viol_freq.loc['78731':'78740']
-viol_freq_4 = viol_freq.loc['78741':'78749']
-viol_freq_5 = viol_freq.loc['78750':'78760']
-
-viol_freq_1.plot.bar(figsize=(20,10), stacked=True, fontsize=14, rot=60)
-plt.show()
-
-viol_freq_2.plot.bar(figsize=(20,10), stacked=True, fontsize=14, rot=60)
-plt.show()
-
-viol_freq_3.plot.bar(figsize=(20,10), stacked=True, fontsize=14, rot=60)
-plt.show()
-
-viol_freq_4.plot.bar(figsize=(20,10), stacked=True, fontsize=14, rot=60)
-plt.show()
-
-viol_freq_5.plot.bar(figsize=(20,10), stacked=True, fontsize=14, rot=60)
+viol_freq.plot.bar(figsize=(25,12.5),
+                  stacked=True,
+                  rot=60,
+                  fontsize=14)
+plt.title('Violent Crime Distribution by Zipcode and Type since 2003')
 plt.show()
 
 
-# In[17]:
+# In[25]:
 
 
-viol_freq.corr()
+fig, ax = plt.subplots()
+ax.plot(df_viol_mur['longitude'], 
+        df_viol_mur['latitude'], 
+        'o', 
+        markersize=1)
+plt.title('Murder & Capital Murder since 2003 Map Plot')
+plt.show()
 
 
+# Needless to say, violent crimes go hand-in-hand with other violent crimes.
+# 
 # So far, 78753 and 78741 are the top hotspots for all sorts of crime in Austin, including violent crime.
 # 
 # For non-capital murder, 78741 comes in at number 1 with 10.91%. 
@@ -345,8 +354,8 @@ viol_freq.corr()
 # 
 # So, if we're honest, 78753 actually is the number 1 hotspot for murder because it has played host to 40 non-capital murders and 11 capital murders in total, meaning it has hosted 11% of all murders. 78741, if we combine all murders, only accounts for 10.56% of the total since 2003. 
 # 
-# Next, 78741 climbs back to claim the number 1 spot for rape at 12.09% -- 3.43 percentage points higher than the number 2 spot 78753 carrying 8.66% which is quite a significant lead when you look at it on the graph!! Why does rape occur so much more often in this area than in others? 
+# Next, 78741 climbs back to claim the number 1 spot for rape at 12.09% -- 3.43 percentage points higher than the number 2 spot 78753 carrying 8.66% which is quite a significant lead when you look at it on the graph!! Why does rape occur so much more often in this area than in others?
 # 
+# What also stands out zipcode 78731. Although violent crime frequencies are among the lowest, it has an enormously higher percentage of rape crimes than any other zipcode (over 50%). In fact, rape occurs at a higher percentage in that zipcode than aggrivated assault. Why is that? 
 # 
-# 
-# In the next part of the analysis, we'll look even closer at all the above data... 
+# Breaking down violent crime by month, we see that May has the most number of aggrivated assaults, while June sees the most number of rapes, and July sees the most non-capital murders. For capital murders, November is the month when they are most likely to occur. 
